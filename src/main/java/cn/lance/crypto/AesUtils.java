e8d58452c0a08c08d81a393ea3784df396afb8d7
Lance Tsai <7ancetsai@gmail.com>
2025-04-03 21:56:16 +0800

    feat: update AesUtils
    - Add AES CBC mode method

--- src/main/java/cn/lance/crypto/AesUtils.java
+++ src/main/java/cn/lance/crypto/AesUtils.java
@@ -4,17 +4,30 @@ import org.apache.commons.codec.DecoderException;
 import org.apache.commons.codec.binary.Hex;
 
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.util.Objects;
 
 /**
  * Advanced Encryption Standard
+ *
+ * @implNote Support two modes:
+ * 1. Electronic Codebook Mode (For security reasons, not recommended)
+ * 2. Cipher Block Chaining Mode
  */
 public class AesUtils {
 
     private final static String ALGORITHM = "AES";
+
+    /**
+     * algorithm/mode/padding
+     */
+    private final static String TRANSFORMATION = "AES/CBC/PKCS5Padding";
+
     private final static Integer KEY_SIZE = 256;
 
     private AesUtils() {
@@ -23,7 +36,7 @@ public class AesUtils {
     /**
      * 生成AES密钥
      *
-     * @return AES密钥
+     * @return AES密钥（十六进制）
      */
     public static String generateKey() {
         KeyGenerator keyGenerator;
@@ -39,7 +52,19 @@ public class AesUtils {
     }
 
     /**
-     * AES加密
+     * 生成AES初始向量
+     *
+     * @return AES初始向量（十六进制）
+     */
+    public static String generateIv() {
+        // AES块大小是128位，即16字节
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new String(Hex.encodeHex(iv));
+    }
+
+    /**
+     * AES加密（ECB模式）
      *
      * @param key       密钥（十六进制）
      * @param plaintext 原文
@@ -74,7 +99,7 @@ public class AesUtils {
     }
 
     /**
-     * AES解密
+     * AES解密（ECB模式）
      *
      * @param key        密钥（十六进制）
      * @param ciphertext 密文（十六进制）
@@ -99,6 +124,84 @@ public class AesUtils {
         // init cipher step2
         SecretKeySpec secretKeySpec = new SecretKeySpec(byteKey, ALGORITHM);
         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+
+        // process encryptedStr
+        byte[] byteEncryptedStr;
+        byteEncryptedStr = Hex.decodeHex(ciphertext);
+
+        byte[] bytes;
+        bytes = cipher.doFinal(byteEncryptedStr);
+
+        return new String(bytes);
+    }
+
+    /**
+     * AES加密（CBC模式）
+     *
+     * @param key       密钥（十六进制）
+     * @param iv        初始向量（十六进制）
+     * @param plaintext 原文
+     * @return 密文
+     */
+    public static String encrypt(String key, String iv, String plaintext) throws DecoderException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Objects.requireNonNull(key, "key must not be null");
+        Objects.requireNonNull(key, "iv must not be null");
+        Objects.requireNonNull(plaintext, "plaintext must not be null");
+
+        // init cipher step1
+        Cipher cipher;
+        try {
+            cipher = Cipher.getInstance(TRANSFORMATION);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        }
+
+        // process key
+        byte[] byteKey = Hex.decodeHex(key);
+        byte[] ivKey = Hex.decodeHex(iv);
+
+        // init cipher step2
+        SecretKeySpec secretKeySpec = new SecretKeySpec(byteKey, ALGORITHM);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(ivKey);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+
+        // encrypt
+        byte[] bytes;
+        bytes = cipher.doFinal(plaintext.getBytes());
+
+        // encode result
+        return new String(Hex.encodeHex(bytes));
+    }
+
+    /**
+     * AES解密（CBC模式）
+     *
+     * @param key        密钥（十六进制）
+     * @param iv         初始向量（十六进制）
+     * @param ciphertext 密文
+     * @return 原文
+     */
+    public static String decrypt(String key, String iv, String ciphertext) throws DecoderException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+        Objects.requireNonNull(key, "key must not be null");
+        Objects.requireNonNull(key, "iv must not be null");
+        Objects.requireNonNull(ciphertext, "ciphertext must not be null");
+
+        // init cipher step1
+        Cipher cipher;
+        try {
+            cipher = Cipher.getInstance(TRANSFORMATION);
+        } catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+            throw new RuntimeException(e);
+        }
+
+        // process key
+        byte[] byteKey = Hex.decodeHex(key);
+        byte[] ivKey = Hex.decodeHex(iv);
+
+        // init cipher step2
+        SecretKeySpec secretKeySpec = new SecretKeySpec(byteKey, ALGORITHM);
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(ivKey);
+        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
 
         // process encryptedStr
         byte[] byteEncryptedStr;
--- src/test/java/cn/lance/crypto/AesUtilsTest.java
+++ src/test/java/cn/lance/crypto/AesUtilsTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
+import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 
 public class AesUtilsTest {
@@ -17,19 +18,13 @@ public class AesUtilsTest {
     }
 
     @Test
-    public void testEncrypt() throws DecoderException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
-        String key = AesUtils.generateKey();
-        System.out.println("AES key: " + key);
-
-        String plaintext = "Hello, AES!";
-        System.out.println("Plaintext: " + plaintext);
-
-        String ciphertext = AesUtils.encrypt(key, plaintext);
-        System.out.println("AES ciphertext: " + ciphertext);
+    public void testGenerateIv() {
+        String iv = AesUtils.generateIv();
+        System.out.println("AES iv: " + iv);
     }
 
     @Test
-    public void textDecrypt() throws DecoderException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+    public void testEncryptWithECB() throws DecoderException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
         String key = AesUtils.generateKey();
         System.out.println("AES key: " + key);
 
@@ -37,13 +32,62 @@ public class AesUtilsTest {
         System.out.println("Plaintext: " + plaintext);
 
         String ciphertext = AesUtils.encrypt(key, plaintext);
-        System.out.println("AES ciphertext: " + ciphertext);
+        System.out.println("AES ECB ciphertext: " + ciphertext);
+    }
+
+    @Test
+    public void textDecryptWithECB() throws DecoderException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException {
+        String key = AesUtils.generateKey();
+        System.out.println("AES key: " + key);
+
+        String plaintext = "Hello, AES!";
+        System.out.println("Plaintext: " + plaintext);
+
+        String ciphertext = AesUtils.encrypt(key, plaintext);
+        System.out.println("AES ECB ciphertext: " + ciphertext);
 
         String decryptedText = AesUtils.decrypt(key, ciphertext);
-        System.out.println("AES decryptedText: " + decryptedText);
+        System.out.println("AES ECB decryptedText: " + decryptedText);
 
         boolean result = plaintext.equals(decryptedText);
-        System.out.println("Plaintext equals to decryptedText? " + result);
+        System.out.println("ECB Plaintext equals to decryptedText? " + result);
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void testEncryptWithCBC() throws DecoderException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
+        String key = AesUtils.generateKey();
+        System.out.println("AES key: " + key);
+
+        String iv = AesUtils.generateIv();
+        System.out.println("AES iv: " + iv);
+
+        String plaintext = "Hello, AES!";
+        System.out.println("Plaintext: " + plaintext);
+
+        String ciphertext = AesUtils.encrypt(key, iv, plaintext);
+        System.out.println("AES CBC ciphertext: " + ciphertext);
+    }
+
+    @Test
+    public void textDecryptWithCBC() throws DecoderException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
+        String key = AesUtils.generateKey();
+        System.out.println("AES key: " + key);
+
+        String iv = AesUtils.generateIv();
+        System.out.println("AES iv: " + iv);
+
+        String plaintext = "Hello, AES!";
+        System.out.println("Plaintext: " + plaintext);
+
+        String ciphertext = AesUtils.encrypt(key, iv, plaintext);
+        System.out.println("AES CBC ciphertext: " + ciphertext);
+
+        String decryptedText = AesUtils.decrypt(key, iv, ciphertext);
+        System.out.println("AES CBC decryptedText: " + decryptedText);
+
+        boolean result = plaintext.equals(decryptedText);
+        System.out.println("CBC Plaintext equals to decryptedText? " + result);
         Assertions.assertTrue(result);
     }
 
